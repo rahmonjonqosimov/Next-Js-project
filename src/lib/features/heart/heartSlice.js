84926c269fda17f3f7826157f24362659cd5588e
QosimovRahmonjonn <qosimovrahmonjon2023@gmail.com>
2024-05-29 20:47:23 +0500

    qri

--- src/lib/features/heart/heartSlice.js
+++ src/lib/features/heart/heartSlice.js
@@ -1,13 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  value: JSON.parse(localStorage.getItem("wishlist")) || [],
+  value: [],
 };
 
 const heartSlice = createSlice({
   name: "heart",
   initialState,
   reducers: {
+    initializeHeart: (state) => {
+      if (typeof window !== "undefined") {
+        const wishlist = localStorage.getItem("wishlist");
+        state.value = wishlist ? JSON.parse(wishlist) : [];
+      }
+    },
     toggleHeart: (state, action) => {
       let index = state.value.findIndex((i) => i.id === action.payload.id);
       if (index < 0) {
@@ -15,10 +21,12 @@ const heartSlice = createSlice({
       } else {
         state.value = state.value.filter((i) => i.id !== action.payload.id);
       }
-      localStorage.setItem("wishlist", JSON.stringify(state.value));
+      if (typeof window !== "undefined") {
+        localStorage.setItem("wishlist", JSON.stringify(state.value));
+      }
     },
   },
 });
 
-export const { toggleHeart } = heartSlice.actions;
+export const { initializeHeart, toggleHeart } = heartSlice.actions;
 export default heartSlice.reducer;
